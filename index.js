148352a471c2d0f71d4f4c11606cb0628b3fa89a
romeo131 <romeo131@gmail.com>
2013-12-18 17:29:15 -0800

    ~  add

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 var http = require('http');
 http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello Mr.Karl\n');
+    res.end('Hello Mr.Karl~\n');
 //}).listen(8080, "127.0.0.1");
 }).listen(8080, "192.168.56.101");
 
