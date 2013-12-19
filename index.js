92cd5e79f19469c1e2784449957e072c37dac751
romeo131 <romeo131@gmail.com>
2013-12-18 16:26:40 -0800

    add index.js(node) file

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by romeo131 on 12/10/13.
+ */
+var http = require('http');
+http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello Karl\n');
+//}).listen(8080, "127.0.0.1");
+}).listen(8080, "192.168.56.101");
+
+console.log('Server running at http://127.0.0.1:8080/');
